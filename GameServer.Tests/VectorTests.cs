1a22b11bcde064460edb8f2ee87e93e117efe25a
sh-zhanslu <zhanslu.shakenova06@mail.ru>
2026-04-01 19:48:31 +0600

    Добавлен класс Vector и его тесты

---
+++ GameServer.Tests/VectorTests.cs
@@ -0,0 +1,51 @@
+﻿using Xunit;
+using System;
+
+namespace GameServer.Tests
+{
+    public class VectorTests
+    {
+        [Fact]
+        public void Add_VectorsSameDimension_ReturnsSum()
+        {
+            var v1 = new Vector(1, -1, 2);
+            var v2 = new Vector(-1, 1, -2);
+            var result = v1.Add(v2);
+            Assert.Equal(new Vector(0, 0, 0), result);
+        }
+
+        [Fact]
+        public void Add_VectorsDifferentDimension_Throws()
+        {
+            var v1 = new Vector(1, 2, 3);
+            var v2 = new Vector(1, 2);
+            Assert.Throws<ArgumentException>(() => v1.Add(v2));
+            Assert.Throws<ArgumentException>(() => v2.Add(v1));
+        }
+
+        [Fact]
+        public void Equals_SameCoordinates_ReturnsTrue()
+        {
+            var v1 = new Vector(1, 2, 3);
+            var v2 = new Vector(1, 2, 3);
+            Assert.Equal(v1, v2);
+            Assert.True(v1 == v2);
+        }
+
+        [Fact]
+        public void Equals_DifferentCoordinates_ReturnsFalse()
+        {
+            var v1 = new Vector(1, 2, 3);
+            var v2 = new Vector(1, 2, 4);
+            Assert.NotEqual(v1, v2);
+            Assert.True(v1 != v2);
+        }
+
+        [Fact]
+        public void GetHashCode_ReturnsHashCode()
+        {
+            var v = new Vector(1, 2, 3);
+            Assert.IsType<int>(v.GetHashCode());
+        }
+    }
+}
---
+++ GameServer/Vector.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer
+{
+    public class Vector
+    {
+        private readonly int[] _coordinates;
+
+        public Vector(params int[] coordinates)
+        {
+            if (coordinates == null)
+                throw new ArgumentNullException(nameof(coordinates));
+            _coordinates = coordinates.ToArray();
+        }
+
+        public int Dimension => _coordinates.Length;
+
+        public int this[int index] => _coordinates[index];
+
+        public Vector Add(Vector other)
+        {
+            if (other.Dimension != Dimension)
+                throw new ArgumentException("Dimensions must match.");
+            var newCoords = new int[Dimension];
+            for (int i = 0; i < Dimension; i++)
+                newCoords[i] = _coordinates[i] + other._coordinates[i];
+            return new Vector(newCoords);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Vector other)
+            {
+                if (Dimension != other.Dimension)
+                    return false;
+                for (int i = 0; i < Dimension; i++)
+                    if (_coordinates[i] != other._coordinates[i])
+                        return false;
+                return true;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 17;
+            foreach (var coord in _coordinates)
+                hash = hash * 31 + coord.GetHashCode();
+            return hash;
+        }
+
+        public static Vector operator +(Vector a, Vector b) => a.Add(b);
+        public static bool operator ==(Vector a, Vector b) => Equals(a, b);
+        public static bool operator !=(Vector a, Vector b) => !Equals(a, b);
+    }
+}
